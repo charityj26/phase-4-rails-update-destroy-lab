9cc3b34471fd66667b44f26023c2766a4f05794e
charityj26 <charity.joseph@student.moringaschool.com>
2023-07-02 21:52:49 +0300

    completed assignment

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,4 +1,5 @@
 class PlantsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   # GET /plants
   def index
@@ -8,8 +9,10 @@ class PlantsController < ApplicationController
 
   # GET /plants/:id
   def show
-    plant = Plant.find_by(id: params[:id])
+    plant = find_plant
     render json: plant
+    rescue ActiveRecord::RecordNotFound
+    render_not_found_response
   end
 
   # POST /plants
@@ -18,9 +21,26 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  def update
+    plant = find_plant
+    plant.update(plant_params)
+    render json: plant
+    rescue ActiveRecord::RecordNotFound
+      render_not_found_response
+  end
+
+  def destroy
+    plant = find_plant
+    plant.destroy
+    head :no_content
+  end
+
   private
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
+  end
+  def find_plant
+    Plant.find(params[:id])
   end
 end
